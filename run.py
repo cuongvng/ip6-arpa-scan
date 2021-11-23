2651aa0be1f29ed0841e37264db049dca28ee944
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-11-23 07:28:27 -0500

    automate reverse lookuo automate reverse lookup

--- run.py
+++ run.py
@@ -1,31 +1,28 @@
 import subprocess
+from sys import prefix
 import pandas as pd
 from ip6_arpa_scan import drilldown, reverse_ipv6
+from glob import glob
 
 DNS_SERVER = "8.8.8.8"
-FILES = [
-	# "./data/apnic-ipv6-AU.csv",
-	# "./data/apnic-ipv6-SG.csv",
-	# "./data/apnic-ipv6-IN.csv",
-	"./data/apnic-ipv6-JP.csv",
-	"./data/apnic-ipv6-KR.csv",
-]
+COUNTRIES = glob("./data/*.csv")
 
 def main():
-	for file in FILES:
-		print(file)
+	for c in COUNTRIES[:5]:
+		print(c)
 
-		df = pd.read_csv(file)
+		df = pd.read_csv(c)
 		df = df[df["Status"] == "Assigned"]
 		df = df[["Range start", "Prefix"]]
 		
 		with open("result.txt", 'a') as fw:
-			fw.write(f'\n{file}\n')
+			fw.write(f'\n{c}\n')
 			fw.close()
 
 		for row in df.iterrows():
 			addr = row[1]["Range start"]
-			subprocess.run(["python", "./ip6_arpa_scan.py", addr, DNS_SERVER])			
+			prefix = str(row[1]["Prefix"])
+			subprocess.run(["python", "./ip6_arpa_scan.py", addr, DNS_SERVER, prefix])			
 
 if __name__ == '__main__':
 	main()
